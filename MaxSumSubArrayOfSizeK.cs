c9d7d21c46f12172ef9ca04de1e27692e2288979
Mohansakthivel Rajasekaran <44009298+mohansakthivel@users.noreply.github.com>
2022-10-02 11:21:10 -0400

    Update MaxSumSubArrayOfSizeK.cs

--- MaxSumSubArrayOfSizeK.cs
+++ MaxSumSubArrayOfSizeK.cs
@@ -2,6 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+// Maximum Sum Subarray of Size K (easy)
+// Input: [2, 1, 5, 1, 3, 2], k=3 
+// Output: 9
+// Explanation: Subarray with maximum sum is [5, 1, 3].
+
+
 class MaxSumSubArrayOfSizeK {
     
     public static int findMaxSumSubArray(int k, int[] arr)      
